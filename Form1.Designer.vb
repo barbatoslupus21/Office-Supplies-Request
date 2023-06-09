49bfaf3d078e4f9d10150bf50380cf727e97ae19
User <User@MIS-ZN>
2023-06-09 08:15:32 +0800

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,310 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As ComponentModel.ComponentResourceManager = New ComponentModel.ComponentResourceManager(GetType(Form1))
+        DataGridView1 = New DataGridView()
+        Supplies = New GroupBox()
+        txtsearch = New TextBox()
+        Label1 = New Label()
+        GroupBox1 = New GroupBox()
+        Label3 = New Label()
+        btnremove = New Button()
+        txtrs = New TextBox()
+        datepicker = New DateTimePicker()
+        Label4 = New Label()
+        txtrqty = New TextBox()
+        btncancel = New Button()
+        Label2 = New Label()
+        btnclear = New Button()
+        txtrby = New TextBox()
+        btnsubmit = New Button()
+        Label5 = New Label()
+        txtline = New TextBox()
+        Label8 = New Label()
+        DataGridView2 = New DataGridView()
+        btnsubmit2 = New Button()
+        CType(DataGridView1, ComponentModel.ISupportInitialize).BeginInit()
+        Supplies.SuspendLayout()
+        GroupBox1.SuspendLayout()
+        CType(DataGridView2, ComponentModel.ISupportInitialize).BeginInit()
+        SuspendLayout()
+        ' 
+        ' DataGridView1
+        ' 
+        DataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        DataGridView1.Location = New Point(6, 75)
+        DataGridView1.Name = "DataGridView1"
+        DataGridView1.RowHeadersWidth = 51
+        DataGridView1.RowTemplate.Height = 29
+        DataGridView1.Size = New Size(628, 256)
+        DataGridView1.TabIndex = 0
+        ' 
+        ' Supplies
+        ' 
+        Supplies.Controls.Add(txtsearch)
+        Supplies.Controls.Add(Label1)
+        Supplies.Controls.Add(DataGridView1)
+        Supplies.Font = New Font("Century Gothic", 12F, FontStyle.Regular, GraphicsUnit.Point)
+        Supplies.Location = New Point(12, 12)
+        Supplies.Name = "Supplies"
+        Supplies.Size = New Size(640, 337)
+        Supplies.TabIndex = 2
+        Supplies.TabStop = False
+        Supplies.Text = "Supplies"
+        ' 
+        ' txtsearch
+        ' 
+        txtsearch.Location = New Point(186, 37)
+        txtsearch.Name = "txtsearch"
+        txtsearch.Size = New Size(448, 32)
+        txtsearch.TabIndex = 3
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Font = New Font("Century Gothic", 12F, FontStyle.Regular, GraphicsUnit.Point)
+        Label1.Location = New Point(15, 40)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(89, 23)
+        Label1.TabIndex = 0
+        Label1.Text = "Material"
+        ' 
+        ' GroupBox1
+        ' 
+        GroupBox1.Controls.Add(Label3)
+        GroupBox1.Controls.Add(btnremove)
+        GroupBox1.Controls.Add(txtrs)
+        GroupBox1.Controls.Add(datepicker)
+        GroupBox1.Controls.Add(Label4)
+        GroupBox1.Controls.Add(txtrqty)
+        GroupBox1.Controls.Add(btncancel)
+        GroupBox1.Controls.Add(Label2)
+        GroupBox1.Controls.Add(btnclear)
+        GroupBox1.Controls.Add(txtrby)
+        GroupBox1.Controls.Add(btnsubmit)
+        GroupBox1.Controls.Add(Label5)
+        GroupBox1.Controls.Add(txtline)
+        GroupBox1.Controls.Add(Label8)
+        GroupBox1.Font = New Font("Century Gothic", 12F, FontStyle.Regular, GraphicsUnit.Point)
+        GroupBox1.Location = New Point(12, 355)
+        GroupBox1.Name = "GroupBox1"
+        GroupBox1.Size = New Size(640, 322)
+        GroupBox1.TabIndex = 9
+        GroupBox1.TabStop = False
+        GroupBox1.Text = "Requestition Form"
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Font = New Font("Century Gothic", 12F, FontStyle.Regular, GraphicsUnit.Point)
+        Label3.Location = New Point(42, 66)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(117, 23)
+        Label3.TabIndex = 16
+        Label3.Text = "RS Number"
+        ' 
+        ' btnremove
+        ' 
+        btnremove.Location = New Point(432, 255)
+        btnremove.Name = "btnremove"
+        btnremove.Size = New Size(114, 44)
+        btnremove.TabIndex = 17
+        btnremove.Text = "REMOVE"
+        btnremove.UseVisualStyleBackColor = True
+        ' 
+        ' txtrs
+        ' 
+        txtrs.BackColor = SystemColors.HighlightText
+        txtrs.Enabled = False
+        txtrs.ForeColor = Color.Black
+        txtrs.Location = New Point(249, 66)
+        txtrs.Name = "txtrs"
+        txtrs.Size = New Size(337, 32)
+        txtrs.TabIndex = 15
+        ' 
+        ' datepicker
+        ' 
+        datepicker.CalendarMonthBackground = SystemColors.MenuBar
+        datepicker.Location = New Point(249, 31)
+        datepicker.Name = "datepicker"
+        datepicker.Size = New Size(337, 32)
+        datepicker.TabIndex = 14
+        ' 
+        ' Label4
+        ' 
+        Label4.AutoSize = True
+        Label4.Font = New Font("Century Gothic", 12F, FontStyle.Regular, GraphicsUnit.Point)
+        Label4.Location = New Point(43, 31)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(59, 23)
+        Label4.TabIndex = 13
+        Label4.Text = "Date"
+        ' 
+        ' txtrqty
+        ' 
+        txtrqty.BackColor = SystemColors.Window
+        txtrqty.Font = New Font("Century Gothic", 16.2F, FontStyle.Regular, GraphicsUnit.Point)
+        txtrqty.Location = New Point(249, 133)
+        txtrqty.Name = "txtrqty"
+        txtrqty.Size = New Size(337, 41)
+        txtrqty.TabIndex = 12
+        ' 
+        ' btncancel
+        ' 
+        btncancel.Location = New Point(312, 255)
+        btncancel.Name = "btncancel"
+        btncancel.Size = New Size(114, 44)
+        btncancel.TabIndex = 10
+        btncancel.Text = "CANCEL"
+        btncancel.UseVisualStyleBackColor = True
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Font = New Font("Century Gothic", 12F, FontStyle.Regular, GraphicsUnit.Point)
+        Label2.Location = New Point(43, 104)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(176, 23)
+        Label2.TabIndex = 11
+        Label2.Text = "Line/Department"
+        ' 
+        ' btnclear
+        ' 
+        btnclear.Location = New Point(192, 255)
+        btnclear.Name = "btnclear"
+        btnclear.Size = New Size(114, 44)
+        btnclear.TabIndex = 9
+        btnclear.Text = "CLEAR"
+        btnclear.UseVisualStyleBackColor = True
+        ' 
+        ' txtrby
+        ' 
+        txtrby.BackColor = SystemColors.Window
+        txtrby.Location = New Point(249, 176)
+        txtrby.Multiline = True
+        txtrby.Name = "txtrby"
+        txtrby.Size = New Size(337, 65)
+        txtrby.TabIndex = 6
+        ' 
+        ' btnsubmit
+        ' 
+        btnsubmit.Location = New Point(72, 255)
+        btnsubmit.Name = "btnsubmit"
+        btnsubmit.Size = New Size(114, 44)
+        btnsubmit.TabIndex = 8
+        btnsubmit.Text = "SUBMIT"
+        btnsubmit.UseVisualStyleBackColor = True
+        ' 
+        ' Label5
+        ' 
+        Label5.AutoSize = True
+        Label5.Font = New Font("Century Gothic", 12F, FontStyle.Regular, GraphicsUnit.Point)
+        Label5.Location = New Point(42, 180)
+        Label5.Name = "Label5"
+        Label5.Size = New Size(144, 23)
+        Label5.TabIndex = 5
+        Label5.Text = "Requested By"
+        ' 
+        ' txtline
+        ' 
+        txtline.BackColor = SystemColors.Window
+        txtline.Location = New Point(249, 100)
+        txtline.Name = "txtline"
+        txtline.Size = New Size(337, 32)
+        txtline.TabIndex = 3
+        ' 
+        ' Label8
+        ' 
+        Label8.AutoSize = True
+        Label8.Font = New Font("Century Gothic", 12F, FontStyle.Regular, GraphicsUnit.Point)
+        Label8.Location = New Point(43, 137)
+        Label8.Name = "Label8"
+        Label8.Size = New Size(157, 23)
+        Label8.TabIndex = 0
+        Label8.Text = "Requested Qty"
+        ' 
+        ' DataGridView2
+        ' 
+        DataGridView2.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        DataGridView2.Location = New Point(658, 12)
+        DataGridView2.Name = "DataGridView2"
+        DataGridView2.RowHeadersWidth = 51
+        DataGridView2.RowTemplate.Height = 29
+        DataGridView2.Size = New Size(1128, 624)
+        DataGridView2.TabIndex = 10
+        ' 
+        ' btnsubmit2
+        ' 
+        btnsubmit2.Location = New Point(1672, 637)
+        btnsubmit2.Name = "btnsubmit2"
+        btnsubmit2.Size = New Size(114, 44)
+        btnsubmit2.TabIndex = 17
+        btnsubmit2.Text = "SUBMIT"
+        btnsubmit2.UseVisualStyleBackColor = True
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackColor = SystemColors.Window
+        ClientSize = New Size(1798, 686)
+        Controls.Add(btnsubmit2)
+        Controls.Add(DataGridView2)
+        Controls.Add(GroupBox1)
+        Controls.Add(Supplies)
+        Icon = CType(resources.GetObject("$this.Icon"), Icon)
+        Name = "Form1"
+        StartPosition = FormStartPosition.CenterScreen
+        Text = "Office Supplies Request"
+        CType(DataGridView1, ComponentModel.ISupportInitialize).EndInit()
+        Supplies.ResumeLayout(False)
+        Supplies.PerformLayout()
+        GroupBox1.ResumeLayout(False)
+        GroupBox1.PerformLayout()
+        CType(DataGridView2, ComponentModel.ISupportInitialize).EndInit()
+        ResumeLayout(False)
+    End Sub
+
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents Supplies As GroupBox
+    Friend WithEvents txtsearch As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents btncancel As Button
+    Friend WithEvents btnclear As Button
+    Friend WithEvents btnsubmit As Button
+    Friend WithEvents txtrby As TextBox
+    Friend WithEvents Label5 As Label
+    Friend WithEvents txtline As TextBox
+    Friend WithEvents Label8 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents datepicker As DateTimePicker
+    Friend WithEvents Label4 As Label
+    Friend WithEvents txtrqty As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents txtrs As TextBox
+    Friend WithEvents DataGridView2 As DataGridView
+    Friend WithEvents btnsubmit2 As Button
+    Friend WithEvents btnremove As Button
+End Class
---
+++ Form1.vb
@@ -0,0 +1,239 @@
+﻿Imports MySql.Data.MySqlClient
+Imports Microsoft.VisualBasic.ApplicationServices
+Imports Microsoft.Win32
+Imports System.Transactions
+Imports System.Windows.Forms.Design
+Imports System.Windows.Forms.VisualStyles.VisualStyleElement.Status
+Imports OfficeOpenXml
+Imports System.IO
+Imports System.ComponentModel
+Imports System.Windows
+Imports System.Windows.Forms.VisualStyles.VisualStyleElement
+
+Public Class Form1
+
+    Dim sqlconn As New MySqlConnection
+    Dim sqlCmd As New MySqlCommand
+    Dim sqlRd As MySqlDataReader
+    Dim sqlDt As New DataTable
+    Dim DtA As New MySqlDataAdapter
+    Dim sqlquery As String
+
+    Dim server As String = "localhost"
+    Dim username As String = "root"
+    Dim password As String = "tekkadan"
+    Dim database As String = "office_supplies"
+    Dim tableName As String = "office_supplies_price"
+    Dim bindingSource As New BindingSource()
+    Private Bitmap As Bitmap
+    Private filePath As String = Path.Combine(Application.StartupPath, "requestNumber.txt")
+    Public Shared BindingList1 As New BindingList(Of DataRow)
+
+    Private Sub updatetable()
+        sqlconn.ConnectionString = "server=" + server + ";" + "user id=" + username + ";" _
+        + "password=" + password + ";" + "database=" + database
+
+        sqlconn.Open()
+        sqlCmd.Connection = sqlconn
+        sqlCmd.CommandText = "Select Code, Part_number, Description, Unit from office_supplies.office_supplies_price LIMIT 1000"
+
+        sqlRd = sqlCmd.ExecuteReader
+        sqlDt.Load(sqlRd)
+        sqlRd.Close()
+        sqlconn.Close()
+        DataGridView1.DataSource = sqlDt
+    End Sub
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        updatetable()
+        sqlconn.ConnectionString = "server=" + server + ";" + "user id=" + username + ";" _
+    + "password=" + password + ";" + "database=" + database
+
+        sqlconn.Open()
+        sqlCmd.Connection = sqlconn
+        sqlCmd.CommandText = "Select Code, Part_number, Description, Unit from office_supplies.office_supplies_price"
+
+        sqlRd = sqlCmd.ExecuteReader
+        sqlDt.Load(sqlRd)
+        sqlRd.Close()
+        sqlconn.Close()
+        bindingSource.DataSource = sqlDt
+        DataGridView1.DataSource = bindingSource
+        DataGridView1.Columns(0).Width = 80
+        DataGridView1.Columns(1).Width = 200
+        DataGridView1.Columns(2).Width = 180
+        DataGridView1.Columns(3).Width = 120
+        DataGridView1.AllowUserToAddRows = False
+
+        DataGridView2.Columns.Add("Date", "Date")
+        DataGridView2.Columns.Add("RS_Number", "RS_Number")
+        DataGridView2.Columns.Add("Requested_By", "Requested_By")
+        DataGridView2.Columns.Add("Line", "Line")
+        DataGridView2.Columns.Add("Description", "Description")
+        DataGridView2.Columns.Add("Part_Number", "Part_Number")
+        DataGridView2.Columns.Add("Unit", "Unit")
+        DataGridView2.Columns.Add("Quantity", "Quantity")
+        DataGridView2.Columns(0).Width = 150
+        DataGridView2.Columns(1).Width = 110
+        DataGridView2.Columns(2).Width = 200
+        DataGridView2.Columns(3).Width = 150
+        DataGridView2.Columns(4).Width = 250
+        DataGridView2.Columns(5).Width = 250
+        DataGridView2.Columns(6).Width = 80
+        DataGridView2.Columns(7).Width = 80
+        DataGridView2.AllowUserToAddRows = False
+        txtsearch.Focus()
+        If File.Exists(filePath) Then
+            ' If the file exists, read the request number from it.
+            Dim text As String = File.ReadAllText(filePath)
+            Dim number As Integer
+            If Int32.TryParse(text, number) Then
+                txtrs.Text = number.ToString()
+            Else
+                ' If the file doesn't contain a valid number, start from 1.
+                txtrs.Text = "1"
+            End If
+        Else
+            ' If the file doesn't exist, start from 1.
+            txtrs.Text = "1"
+        End If
+
+    End Sub
+
+    Private Sub btncancel_Click(sender As Object, e As EventArgs) Handles btncancel.Click
+        Me.Close()
+
+    End Sub
+
+    Private Sub btnclear_Click(sender As Object, e As EventArgs) Handles btnclear.Click
+        txtsearch.Text = ""
+        txtline.Text = ""
+        txtrqty.Text = ""
+        txtrby.Text = ""
+        txtrs.Text = ""
+
+    End Sub
+    Private Sub txtsearch_TextChanged(sender As Object, e As EventArgs) Handles txtsearch.TextChanged
+        bindingSource.Filter = "Convert(Code, 'System.String') LIKE '%" & txtsearch.Text & "%' OR Part_number LIKE '%" & txtsearch.Text & "%'"
+
+    End Sub
+
+    Private Sub btnsubmit_Click(sender As Object, e As EventArgs) Handles btnsubmit.Click
+        ' Check if all TextBoxes are filled
+        If String.IsNullOrWhiteSpace(txtrs.Text) OrElse String.IsNullOrWhiteSpace(txtrby.Text) OrElse String.IsNullOrWhiteSpace(txtline.Text) OrElse String.IsNullOrWhiteSpace(txtrqty.Text) Then
+            MessageBox.Show("Please fill all information before proceeding.", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+            Return
+        End If
+
+        If DataGridView1.SelectedRows.Count > 0 Then
+            Dim row As String() = New String() {datepicker.Value.ToShortDateString(), txtrs.Text, txtrby.Text, txtline.Text, DataGridView1.SelectedRows(0).Cells(2).Value.ToString(), DataGridView1.SelectedRows(0).Cells(1).Value.ToString(), DataGridView1.SelectedRows(0).Cells(3).Value.ToString(), txtrqty.Text}
+            DataGridView2.Rows.Add(row)
+            txtrqty.Text = ""
+        Else
+            MessageBox.Show("Please select a item in the material list first.")
+        End If
+    End Sub
+    Private Sub txtrqty_KeyPress(sender As Object, e As KeyPressEventArgs) Handles txtrqty.KeyPress
+        If Not Char.IsDigit(e.KeyChar) And Not Char.IsControl(e.KeyChar) Then
+            e.Handled = True
+        End If
+    End Sub
+    Private Sub txtrs_KeyDown(sender As Object, e As KeyEventArgs) Handles txtrs.KeyDown
+        If e.KeyCode = Keys.Enter Then
+            txtline.Focus()
+        End If
+    End Sub
+
+    Private Sub txtline_KeyDown(sender As Object, e As KeyEventArgs) Handles txtline.KeyDown
+        If e.KeyCode = Keys.Enter Then
+            txtrqty.Focus()
+        End If
+    End Sub
+    Private Sub txtrqty_KeyDown(sender As Object, e As KeyEventArgs) Handles txtrqty.KeyDown
+        If e.KeyCode = Keys.Enter Then
+            txtrby.Focus()
+        End If
+    End Sub
+    Private Sub txtrby_KeyDown(sender As Object, e As KeyEventArgs) Handles txtrby.KeyDown
+        If e.KeyCode = Keys.Enter Then
+            btnsubmit.Focus()
+        End If
+    End Sub
+    Private Sub txtsearch_KeyDown(sender As Object, e As KeyEventArgs) Handles txtsearch.KeyDown
+        If e.KeyCode = Keys.Enter Then
+            txtrs.Focus()
+        End If
+    End Sub
+
+    Private Sub btnremove_Click(sender As Object, e As EventArgs) Handles btnremove.Click
+        If DataGridView2.SelectedRows.Count > 0 Then
+            For Each row As DataGridViewRow In DataGridView2.SelectedRows
+                If Not row.IsNewRow Then
+                    DataGridView2.Rows.Remove(row)
+                End If
+            Next
+        Else
+            MessageBox.Show("Please select first the entry you want to remove.")
+        End If
+    End Sub
+
+    Private Sub btnsubmit2_Click(sender As Object, e As EventArgs) Handles btnsubmit2.Click
+        Dim number As Integer = Int32.Parse(txtrs.Text)
+        number += 1
+        File.WriteAllText(filePath, number.ToString())
+
+        ' Update the TextBox with the new request number.
+        txtrs.Text = number.ToString()
+
+        sqlconn.ConnectionString = "server=" + server + ";" + "user id=" + username + ";" _
+    + "password=" + password + ";" + "database=" + database
+
+        sqlconn.Open()
+        sqlCmd.Connection = sqlconn
+        Dim transaction As MySqlTransaction = sqlconn.BeginTransaction()
+        sqlCmd.Transaction = transaction
+
+        Try
+            For Each row As DataGridViewRow In DataGridView2.Rows
+                If Not row.IsNewRow Then
+                    Dim dateValue As Date = Date.Parse(row.Cells("Date").Value.ToString())
+                    Dim rsNo As String = row.Cells("RS_Number").Value.ToString()
+                    Dim requestedBy As String = row.Cells("Requested_By").Value.ToString()
+                    Dim line As String = row.Cells("Line").Value.ToString()
+                    Dim description As String = row.Cells("Description").Value.ToString()
+                    Dim partNumber As String = row.Cells("Part_Number").Value.ToString()
+                    Dim unit As String = row.Cells("Unit").Value.ToString()
+                    Dim qty As String = row.Cells("Quantity").Value.ToString()
+
+                    sqlCmd.CommandText = "INSERT INTO `office_supplies`.`request_supplies` (`Date`, `Rs_no.`, `Requested_by`, `Line`, `Description`, `Part_number`, `Unit`, `Qty`) " _
+    + "VALUES (@date, @rsNo, @requestedBy, @line, @description, @partNumber, @unit, @qty)"
+
+                    sqlCmd.Parameters.AddWithValue("@date", dateValue.ToString("yyyy-MM-dd"))
+                    sqlCmd.Parameters.AddWithValue("@rsNo", rsNo)
+                    sqlCmd.Parameters.AddWithValue("@requestedBy", requestedBy)
+                    sqlCmd.Parameters.AddWithValue("@line", line)
+                    sqlCmd.Parameters.AddWithValue("@description", description)
+                    sqlCmd.Parameters.AddWithValue("@partNumber", partNumber)
+                    sqlCmd.Parameters.AddWithValue("@unit", unit)
+                    sqlCmd.Parameters.AddWithValue("@qty", qty)
+
+                    sqlCmd.ExecuteNonQuery()
+
+                    ' Clear the parameters so they can be used again
+                    sqlCmd.Parameters.Clear()
+
+                End If
+            Next
+
+            transaction.Commit()
+            MessageBox.Show("Your request has been submitted successfully.", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information)
+            DataGridView2.Rows.Clear()
+        Catch ex As Exception
+            transaction.Rollback()
+            MessageBox.Show("An error occurred during submission. No data was inserted. Error: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        Finally
+            sqlconn.Close()
+        End Try
+    End Sub
+
+
+End Class
